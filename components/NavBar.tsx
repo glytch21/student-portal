8c81b99e2233f019e1154d5a35b0bdef3c83da6f
glytch21 <hernandezgio16@gmail.com>
2024-03-24 08:19:28 +0800

    update search margin

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -148,14 +148,36 @@ const Navbar: React.FC<NavbarProps> = ({ profileButtonClick, gradesButtonClick, 
               value={searchValue}
               onChange={handleSearchValue}
               placeholder="Search User..."
-              onKeyPress={(e) => {
-                handleSearch()
-              }}
+              onKeyPress={handleSearch}
               onBlur={handleInputBlur}
-              className="w-full border border-gray-300 rounded-lg p-2 text-black"
+              style={{
+                border: '1px solid #ccc',
+                borderRadius: '0.25rem',
+                width: '300px',
+                padding: '0.5rem',
+                color: '#000',
+                marginLeft: '600px',
+                marginRight: '90px' // Adjust the margin-left as needed
+              }}
             />
+
             {isSearching && (
-              <div className="absolute top-full left-0 w-full bg-white text-black p-2 rounded-md mt-1 z-10 border border-black border-b">
+              <div style={{
+                position: 'absolute',
+                top: '100%',
+                left: '0',
+                width: '300px',
+                backgroundColor: 'white',
+                color: 'black',
+                padding: '0.5rem',
+                borderRadius: '0.375rem',
+                marginTop: '0.25rem',
+                zIndex: '10',
+                border: '1px solid black',
+                borderBottom: 'none',
+                marginLeft: '600px' // Adjust the margin-left as needed
+              }}>
+
                 <div>
                   {resultUsers.map((user: any) => (
                     <div
@@ -198,121 +220,149 @@ const Navbar: React.FC<NavbarProps> = ({ profileButtonClick, gradesButtonClick, 
             )}
           </li>
 
+
           {profileInfoModal && (
             <ProfileInfo user={profileInfo} onClose={handleCloseModal} />
           )}
 
           <li>
-            <button onClick={handleLogout} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Logout</button>
+            <button onClick={handleLogout} className="nav-button p-2 bg-red-500 rounded-lg hover:bg-red-600">Logout</button>
           </li>
 
         </ul>
-      )}
+      )
+      }
 
       {/* nav for teachers */}
 
-      {currentUser && currentUser.role === 'teacher' && (
-        <ul style={{ display: 'flex', listStyleType: 'none', height: '100%', alignItems: 'center' }}>
-          <li style={{ marginRight: '50px', marginLeft: '10px' }}>
-            <p className='font-bold text-lg'>Teachers Portal</p>
-          </li>
-          <li style={{ marginRight: '10px' }}>
-            <button onClick={profileButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Profile</button>
-          </li>
-          <li style={{ marginRight: '10px' }}>
-            <button onClick={classButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Class</button>
-          </li>
+      {
+        currentUser && currentUser.role === 'teacher' && (
+          <ul style={{ display: 'flex', listStyleType: 'none', height: '100%', alignItems: 'center' }}>
+            <li style={{ marginRight: '50px', marginLeft: '10px' }}>
+              <p className='font-bold text-lg'>Teachers Portal</p>
+            </li>
+            <li style={{ marginRight: '10px' }}>
+              <button onClick={profileButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Profile</button>
+            </li>
+            <li style={{ marginRight: '10px' }}>
+              <button onClick={classButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Class</button>
+            </li>
 
-          <li className="relative">
-            <input
-              type="text"
-              id="searchValue"
-              value={searchValue}
-              onChange={handleSearchValue}
-              placeholder="Search User..."
-              onKeyPress={(e) => {
-                handleSearch()
-              }}
-              onBlur={handleInputBlur}
-              className="w-full border border-gray-300 rounded-lg p-2 text-black"
-            />
-            {isSearching && (
-              <div className="absolute top-full left-0 w-full bg-white text-black p-2 rounded-md mt-1 z-10 border border-black border-b">
-                <div>
-                  {resultUsers.map((user: any) => (
-                    <div
-                      key={user.id}
-                      className="bg-white text-black p-2 rounded-md border border-black cursor-pointer flex items-center"
-                      onClick={() => handleOpenModal(user)}
-                    >
-                      <img
-                        src={`https://tfvmclypbhyhkgxjmuid.supabase.co/storage/v1/object/public/images/${user.profile_image}`}
-                        alt="Profile Picture"
-                        className="object-contain rounded-full shadow-lg w-10 h-10 mr-4"
-                        style={{ fontSize: '1.5em' }}
-                      />
-                      <div>
-                        <div className="mb-1" style={{ fontSize: '13px' }}>{user.first_name} {user.last_name}</div>
+            <li className="relative">
+              <input
+                type="text"
+                id="searchValue"
+                value={searchValue}
+                onChange={handleSearchValue}
+                placeholder="Search User..."
+                onKeyPress={handleSearch}
+                onBlur={handleInputBlur}
+                style={{
+                  border: '1px solid #ccc',
+                  borderRadius: '0.25rem',
+                  width: '300px',
+                  padding: '0.5rem',
+                  color: '#000',
+                  marginLeft: '600px',
+                  marginRight: '90px' // Adjust the margin-left as needed
+                }}
+              />
+
+              {isSearching && (
+                <div style={{
+                  position: 'absolute',
+                  top: '100%',
+                  left: '0',
+                  width: '300px',
+                  backgroundColor: 'white',
+                  color: 'black',
+                  padding: '0.5rem',
+                  borderRadius: '0.375rem',
+                  marginTop: '0.25rem',
+                  zIndex: '10',
+                  border: '1px solid black',
+                  borderBottom: 'none',
+                  marginLeft: '600px' // Adjust the margin-left as needed
+                }}>
+
+                  <div>
+                    {resultUsers.map((user: any) => (
+                      <div
+                        key={user.id}
+                        className="bg-white text-black p-2 rounded-md border border-black cursor-pointer flex items-center"
+                        onClick={() => handleOpenModal(user)}
+                      >
+                        <img
+                          src={`https://tfvmclypbhyhkgxjmuid.supabase.co/storage/v1/object/public/images/${user.profile_image}`}
+                          alt="Profile Picture"
+                          className="object-contain rounded-full shadow-lg w-10 h-10 mr-4"
+                          style={{ fontSize: '1.5em' }}
+                        />
+                        <div>
+                          <div className="mb-1" style={{ fontSize: '13px' }}>{user.first_name} {user.last_name}</div>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
 
-                  {lastnameResults.map((user: any) => (
-                    <div
-                      key={user.id}
-                      className="bg-white text-black p-2 rounded-md border border-black cursor-pointer flex items-center"
-                      onClick={() => handleOpenModal(user)}
-                    >
-                      <img
-                        src={`https://tfvmclypbhyhkgxjmuid.supabase.co/storage/v1/object/public/images/${user.profile_image}`}
-                        alt="Profile Picture"
-                        className="object-contain rounded-full shadow-lg w-10 h-10 mr-4"
-                        style={{ fontSize: '1.5em' }}
-                      />
-                      <div>
-                        <div className="mb-1" style={{ fontSize: '13px' }}>{user.first_name} {user.last_name}</div>
+                    {lastnameResults.map((user: any) => (
+                      <div
+                        key={user.id}
+                        className="bg-white text-black p-2 rounded-md border border-black cursor-pointer flex items-center"
+                        onClick={() => handleOpenModal(user)}
+                      >
+                        <img
+                          src={`https://tfvmclypbhyhkgxjmuid.supabase.co/storage/v1/object/public/images/${user.profile_image}`}
+                          alt="Profile Picture"
+                          className="object-contain rounded-full shadow-lg w-10 h-10 mr-4"
+                          style={{ fontSize: '1.5em' }}
+                        />
+                        <div>
+                          <div className="mb-1" style={{ fontSize: '13px' }}>{user.first_name} {user.last_name}</div>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
 
+                  </div>
                 </div>
-              </div>
+              )}
+            </li>
+
+            {profileInfoModal && (
+              <ProfileInfo user={profileInfo} onClose={handleCloseModal} />
             )}
-          </li>
 
-          {profileInfoModal && (
-            <ProfileInfo user={profileInfo} onClose={handleCloseModal} />
-          )}
+            <li>
+              <button onClick={handleLogout} className="nav-button p-2 bg-red-500 rounded-lg hover:bg-red-600">Logout</button>
+            </li>
 
-          <li>
-            <button onClick={handleLogout} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Logout</button>
-          </li>
-
-        </ul>
-      )}
+          </ul>
+        )
+      }
 
       {/* nav for parents */}
 
-      {currentUser && currentUser.role === 'parent' && (
-        <ul style={{ display: 'flex', listStyleType: 'none', height: '100%', alignItems: 'center' }}>
-          <li style={{ marginRight: '50px', marginLeft: '10px' }}>
-            <p className='font-bold text-lg'>Parent Portal</p>
-          </li>
-          <li style={{ marginRight: '10px' }}>
-            <button onClick={profileButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Profile</button>
-          </li>
-          <li style={{ marginRight: '10px' }}>
-            <button onClick={gradesButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Report Card</button>
-          </li>
+      {
+        currentUser && currentUser.role === 'parent' && (
+          <ul style={{ display: 'flex', listStyleType: 'none', height: '100%', alignItems: 'center' }}>
+            <li style={{ marginRight: '50px', marginLeft: '10px' }}>
+              <p className='font-bold text-lg'>Parent Portal</p>
+            </li>
+            <li style={{ marginRight: '10px' }}>
+              <button onClick={profileButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Profile</button>
+            </li>
+            <li style={{ marginRight: '10px' }}>
+              <button onClick={gradesButtonClick} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Report Card</button>
+            </li>
 
 
 
-          <li>
-            <button onClick={handleLogout} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Logout</button>
-          </li>
-        </ul>
-      )}
-    </nav>
+            <li>
+              <button onClick={handleLogout} className="nav-button p-2 bg-red-500 rounded-lg hover:bg-red-600">Logout</button>
+            </li>
+          </ul>
+        )
+      }
+    </nav >
 
 
   );

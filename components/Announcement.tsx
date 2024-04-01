6fc762fede60e8a21034a7fa5d6094e1fed42045
nayrx <espineda.ryanmiguel.c200571@gmail.com>
2024-04-01 22:30:46 +0800

    added Delete button

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { useState, useEffect } from "react"
 import supabase from '@/config/client';
+import { VscKebabVertical as KebabMenu } from "react-icons/vsc";
 
 const Announcement = ({ role, className }:any) => {
     const [announcement, setAnnouncement] = useState<any>([])
+    const [revealed, setRevealed] = useState<string>()
 
     const capitalizeFirstLetter = (str:any) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
@@ -103,6 +105,24 @@ const Announcement = ({ role, className }:any) => {
         }
     ]
 
+    const revealDelete = (id:string) => {
+        setRevealed(id)
+    }
+
+    const handleDelete = async (id:string) => {
+        const { error } = await supabase
+            .from('announcements_table')
+            .delete()
+            .eq('id', id)
+
+        if (error) {
+            console.log('FetchError:', error)
+        } else {
+            fetchAnnouncementData()
+        }
+
+    }
+
     useEffect(() => {
         fetchAnnouncementData()
     }, [])
@@ -113,13 +133,19 @@ const Announcement = ({ role, className }:any) => {
         <>
             {role === 'admin' && (
             <div className="flex flex-col items-center justify-between top-[6rem] left-[6rem] text-4xl font-bold">
-                <div className="text-base font-normal text-white p-4 bg-cyan-600 rounded-md cursor-pointer hover:bg-cyan-500" onClick={updateAnnouncement}>
-                    Update Announcement
+                <div className="text-xl font-semibold text-white p-4 bg-cyan-600 rounded-md cursor-pointer hover:bg-cyan-500" onClick={updateAnnouncement}>
+                    Announcements
                 </div>
             </div>
             )}
             { announcement.slice().reverse().map((data:any) => (
-                <div className="flex flex-col gap-4 bg-white mt-10 p-10 shadow-lg border border-gray-200 rounded-lg">
+                <div className="flex flex-col gap-4 bg-white mt-10 p-10 shadow-lg border border-gray-200 rounded-lg relative">
+                    <div className="absolute top-3 right-1 rounded-full px-1 py-1 hover:bg-gray-300 cursor-pointer" onClick={() => revealDelete(data.id)}>
+                        <KebabMenu />
+                        <div className={`${revealed === data.id && '!block'} hidden absolute -right-14 bottom-[50%] translate-y-[50%] p-1 bg-gray-300 rounded-md cursor-pointer hover:bg-red-700 hover:text-white`} onClick={() => handleDelete(data.id)} >
+                            Delete
+                        </div>
+                    </div>
                     <div className="font-semibold text-lg text-left">
                         <div className="flex items-center justify-between">
                             {data.headline}
